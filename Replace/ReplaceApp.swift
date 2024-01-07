374629a0d2506c6e16c82f6a7a8d8f156f1fdab7
LORSCHEIDER Julien <julien.lorscheider8@etu.univ-lorraine.fr>
2024-01-07 18:33:38 +0100

    Initial Commit

---
+++ Replace/ReplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplaceApp.swift
+//  Replace
+//
+//  Created by Julien on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
